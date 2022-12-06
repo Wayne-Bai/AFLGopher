fa915d6b777e6039cae159f36a61f21de546fdc9
wayne <bwh.buaa@gmail.com>
2022-12-06 17:31:12 -0600

    Seventh Step: Generate if data with the symbol inside if

---
+++ CFG_phase/generate_if_symbol.py
@@ -0,0 +1,27 @@
+from xlrd import open_workbook
+from xlutils.copy import copy
+import pandas as pd
+
+workbook = open_workbook('data.xls')
+original_data = workbook.sheet_by_index(0)
+excel = copy(workbook)
+data = excel.get_sheet(0)
+rows = workbook.sheets()[0].nrows
+
+if_cluster = pd.read_csv('if_probability.csv')
+# original_if = pd.read_csv('if_with_cluster.csv')
+original_if = pd.read_csv('if_data.csv')
+
+original_if['symbol'] = ''
+
+
+for i in range(original_if.shape[0]):
+    for j in range(0,rows):
+        if_file = original_if.loc[i, 'file']
+        if_line = original_if.loc[i, 'line']
+        original_file = original_data.cell_value(j,0)
+        original_line = original_data.cell_value(j,1)
+        if original_data.cell_value(j, 0) == original_if.loc[i, 'file'] and original_data.cell_value(j, 1) == original_if.loc[i, 'line']:
+            original_if.loc[i, 'symbol'] = original_data.cell_value(j, 2)
+
+original_if.to_csv('if_symbol.csv')
