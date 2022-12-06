84edda3a65139c9cc9fe62ed5a7a03a40de9e665
wayne <bwh.buaa@gmail.com>
2022-12-06 16:03:58 -0600

    Pre-process to find if position

---
+++ Pre-Process/if_source_position.py
@@ -0,0 +1,39 @@
+import os
+
+path ='../libxml2'
+
+def get_filelist(path):
+
+    Filelist = []
+
+    for home, dirs, files in os.walk(path):
+        for filename in files:
+            Filelist.append(os.path.join(home, filename))
+
+    c_file_list = [fn for fn in Filelist if fn.endswith('.c')]
+
+    return c_file_list
+
+if __name__ =="__main__":
+
+    path = '../libxml2'
+    c_file_list = get_filelist(path)
+
+    # print(len(c_file_list))
+    # print(c_file_list)
+    with open('if_source_position.txt', 'a') as w:
+        for i in c_file_list:
+            print(i)
+            # if 'buf.c' in i:
+            count = 0
+            with open(i, 'r', encoding = "ISO-8859-1") as f:
+                # file_name = i.strip().split('/')[-1]
+                for line in f.readlines():
+                    count += 1
+                    if 'if ' in line and '#' not in line and '/' not in line and '\\' not in line and line.strip()[0] != '*':
+                        w.write(i)
+                        w.write('    ')
+                        w.write(str(count))
+                        w.write('\n')
+            f.close()
+    w.close()
