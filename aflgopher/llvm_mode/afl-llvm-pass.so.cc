66a56bd98c38acd8f9b5ab8b75ad54c38535511d
wukefu <524663104@qq.com>
2023-04-03 05:12:48 -0500

    fix dot folder

--- aflgopher/llvm_mode/afl-llvm-pass.so.cc
+++ aflgopher/llvm_mode/afl-llvm-pass.so.cc
@@ -358,6 +358,7 @@ bool AFLCoverage::runOnModule(Module &M) {
 
     /* Create dot-files directory */
     std::string dotfiles(OutDirectory + "/dot-files");
+    sys::fs::create_directory(dotfiles);
     /*if (sys::fs::create_directory(dotfiles)) {
       FATAL("Could not create directory %s.", dotfiles.c_str());
     }*/
